bf67e3897dcdb0dd356719817d409776a9027ab0
ryanolejarz <47618456+ryanolejarz@users.noreply.github.com>
2022-03-19 14:16:52 -0400

    Delete main.py

--- main.py
+++
@@ -1,7 +0,0 @@
-from tests.BaseTest import BaseTest
-
-
-test = BaseTest()
-#print(test.get_all_tradable_asset_pairs())
-print(test.get_tradable_asset_pairs(asset_pair='ADAUSD'))
-# print(test.get_tradable_asset_pairs(asset_pairs=['ADAUSD','SHIBUSD']))
